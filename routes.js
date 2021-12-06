e429be4bf223a59b0ae5f63552df455e267a7c9a
Johannes Weber <johannes@drweber.de>
2021-12-05 16:53:15 -0800

    serve tally at /

--- routes.js
+++ routes.js
@@ -2,11 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
-})
-
-router.get('/tally', (req, res) => {
   res.render('tally');
-});
+})
 
 module.exports = router;
