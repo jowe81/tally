646bafd13b18756c871340b442ac353d9e7f397f
Johannes Weber <johannes@drweber.de>
2021-12-07 14:13:39 -0800

    tally label

--- public/scripts/tally.js
+++ public/scripts/tally.js
@@ -44,11 +44,14 @@ const initUI = (camData) => {
   $("#tallies").empty();
   showInstructions(myTallies.length ? false : true);  
   for (cam of camData) {
+    let camLabel = cam.name;
+    //Add the word "cam" if name is just a number
+    if (parseInt(camLabel) > 0) camLabel = `CAM ${cam.name}`;
     //Show all sources in the dashboard
     $("#dashboard").append(`<div id="db_${cam.id}" class="db-item"><div class="">${cam.name}<div></div>`);
     //Show only myTallies in main tally area
     if (myTallies.includes(cam.id)){
-      $("#tallies").append(`<div id="tl_${cam.id}" class="tl-item"><div class="tl_cam-name">${cam.name}<div></div>`);
+      $("#tallies").append(`<div id="tl_${cam.id}" class="tl-item"><div class="tl_cam-name">${camLabel}<div></div>`);
     }
     //Init the data
     paintTallyChange(cam);
